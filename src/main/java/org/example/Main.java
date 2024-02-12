aab9d2dce0ffb4b7918aeac0869965c8a3414ca3
Maria Yandishinskaya <mary.90_80@list.ru>
2024-02-12 12:11:57 +0300

    added adjustments

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MyArrayList<String> myArrayList = new MyArrayList<>();
+        for (int i = 0; i < 11; i++) {
+            myArrayList.add((int) (Math.random() * 50) + " item");
+        }
+        MyArrayList.bubbleSort(myArrayList);
+        System.out.println(myArrayList);
+
+        myArrayList.add(1, "another item");
+
+        MyArrayList newTypeList = new MyArrayList(myArrayList);
+
+        System.out.println("Collection from constructor with collection of another type");
+        System.out.println(newTypeList);
+
+        System.out.println("----------------");
+        newTypeList.bubbleSort();
+        System.out.println(newTypeList);
+        myArrayList.addAll(newTypeList);
+        myArrayList.remove(1);
+        System.out.println(myArrayList);
+        MyArrayList.bubbleSort(myArrayList);
+        System.out.println(myArrayList);
+
+    }
+}
--- src/main/java/org/example/MyArrayList.java
+++ src/main/java/org/example/MyArrayList.java
@@ -7,13 +7,14 @@ public class MyArrayList<T extends Comparable<T>> {
     private T[] elementData;
     private int size = 0;
     private final static int CAPACITY = 10;
+    private final static double GROW_FACTOR = 1.5D;
 
     public MyArrayList() {
         this.elementData = (T[]) new Comparable[CAPACITY];
     }
 
     public MyArrayList(MyArrayList<T> collection) {
-        this.elementData = (T[]) new Comparable[collection.size * 3 / 2 + 1];
+        this.elementData = (T[]) new Comparable[(int) (collection.size * GROW_FACTOR + 1)];
         for (int i = 0; i < collection.size; i++) {
             this.elementData[i] = collection.get(i);
         }
@@ -25,7 +26,7 @@ public class MyArrayList<T extends Comparable<T>> {
     }
 
     private void grow() {
-        elementData = Arrays.copyOf(elementData, elementData.length * 3 / 2 + 1);
+        elementData = Arrays.copyOf(elementData, (int) (elementData.length * GROW_FACTOR + 1));
     }
 
     public void add(T element) {
@@ -47,20 +48,6 @@ public class MyArrayList<T extends Comparable<T>> {
         size++;
     }
 
-    public boolean addAll(MyArrayList<T> collection) {
-        int newSize = collection.size;
-        if (newSize == 0) {
-            return false;
-        }
-        if (elementData.length - size < newSize) {
-            elementData = Arrays.copyOf(elementData, (size + newSize) * 3 / 2 + 1);
-        }
-        for (int i = 0; i < newSize; i++) {
-            elementData[size++] = collection.elementData[i];
-        }
-        return true;
-    }
-
     public T get(int index) {
         checkIndex(index);
         return elementData[index];
@@ -80,6 +67,20 @@ public class MyArrayList<T extends Comparable<T>> {
         }
         size--;
         return oldElement;
+    }
+
+    public boolean addAll(MyArrayList<T> collection) {
+        int newSize = collection.size;
+        if (newSize == 0) {
+            return false;
+        }
+        if (elementData.length - size < newSize) {
+            elementData = Arrays.copyOf(elementData, (int) ((elementData.length - size + newSize) * GROW_FACTOR + 1));
+        }
+        for (int i = 0; i < newSize; i++) {
+            elementData[size++] = collection.elementData[i];
+        }
+        return true;
     }
 
     public String toString() {
@@ -125,45 +126,5 @@ public class MyArrayList<T extends Comparable<T>> {
 
     static public void bubbleSort(MyArrayList collection) {
         collection.bubbleSort();
-    }
-
-    public static void main(String[] args) {
-        MyArrayList<Integer> firstList = new MyArrayList<>();
-        for (int i = 0; i < 11; i++) {
-            firstList.add((int) (Math.random() * 50));
-        }
-        System.out.println("Start collection:");
-        System.out.println(firstList);
-        MyArrayList.bubbleSort(firstList);
-        System.out.println("Sorted collection:");
-        System.out.println(firstList);
-
-        firstList.add(2, 5555);
-        System.out.println("Added element 5555 at sixth position");
-        System.out.println(firstList);
-
-        MyArrayList<Integer> secondList = new MyArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            secondList.add((int) (Math.random() * 1000));
-        }
-        System.out.println("Second collection:");
-        System.out.println(secondList);
-
-        firstList.addAll(secondList);
-        System.out.println("Added second collection to first collection:");
-        System.out.println(firstList);
-
-        System.out.println("Sixth element: " + firstList.get(5));
-        firstList.remove(5);
-        System.out.println("Sixth element after delete: " + firstList.get(5));
-
-        MyArrayList newTypeList = new MyArrayList(firstList);
-
-        System.out.println("Collection from constructor with collection of another type");
-        System.out.println(newTypeList);
-
-        MyArrayList.bubbleSort(newTypeList);
-        System.out.println("Sorted collection with static method:");
-        System.out.println(newTypeList);
     }
 }
